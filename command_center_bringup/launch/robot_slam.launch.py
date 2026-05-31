f0f0f534e13d341ede70753a9017854da419a7eb
Young-kim12 <qnqbqb56@gmail.com>
2026-05-31 18:33:15 +0900

    add robot.launch.py for raspberry

--- command_center_bringup/launch/robot_slam.launch.py
+++ command_center_bringup/launch/robot_slam.launch.py
@@ -3,34 +3,35 @@ import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+#from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
-from launch.actions import EmitEvent, RegisterEventHandler
-from launch_ros.event_handlers import OnStateTransition
+#from launch.actions import EmitEvent, RegisterEventHandler
+#from launch_ros.event_handlers import OnStateTransition
 from launch_ros.actions import LifecycleNode
-from lifecycle_msgs.msg import Transition
+#from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
     bringup_dir     = get_package_share_directory('command_center_bringup')
     description_dir = get_package_share_directory('command_center_description')
-    lidar_dir       = get_package_share_directory('ydlidar_ros2_driver')
+    #lidar_dir       = get_package_share_directory('ydlidar_ros2_driver')
 
     xacro_path   = os.path.join(description_dir, 'urdf', 'command_center.urdf.xacro')
     params_path  = os.path.join(bringup_dir, 'config', 'params.yaml')
     slam_params  = os.path.join(bringup_dir, 'config', 'slam_params.yaml')
-    nav2_params  = os.path.join(bringup_dir, 'config', 'nav2_params.yaml')
-    lidar_params = os.path.join(lidar_dir, 'params', 'ydlidar.yaml')
+    #nav2_params  = os.path.join(bringup_dir, 'config', 'nav2_params.yaml')
+    #lidar_params = os.path.join(lidar_dir, 'params', 'ydlidar.yaml')
 
     robot_description = xacro.process_file(xacro_path).toxml()
 
     # micro-ROS 에이전트 (STM32 ↔ ROS2 브릿지)
-    micro_ros_agent = ExecuteProcess(
-        cmd=['ros2', 'run', 'micro_ros_agent', 'micro_ros_agent',
-             'serial', '--dev', '/dev/ttyACM0', '-b', '115200',],   # 시간 동기화 모드 추가
-        output='screen'
-    )
+    #micro_ros_agent = ExecuteProcess(
+    #    cmd=['ros2', 'run', 'micro_ros_agent', 'micro_ros_agent',
+    #         'serial', '--dev', '/dev/ttyACM0', '-b', '115200',],   # 시간 동기화 모드 추가
+    #    output='screen'
+    #)
+
 
     # 로봇 상태 퍼블리셔
     robot_state_publisher = Node(
@@ -46,14 +47,14 @@ def generate_launch_description():
     )
 
     # HAL 노드들
-    robot_node = Node(
-        package='command_center_hal',
-        executable='robot_node',
-        name='robot_node',
-        output='screen',
-        parameters=[params_path],
-        #remappings=[('/cmd_vel', '/cmd_vel_nav')]
-    )
+    #robot_node = Node(
+    #    package='command_center_hal',
+    #    executable='robot_node',
+    #    name='robot_node',
+    #    output='screen',
+    #    parameters=[params_path],
+    #    #remappings=[('/cmd_vel', '/cmd_vel_nav')]
+    #)
 
     odometry_node = Node(
         package='command_center_hal',
@@ -64,23 +65,23 @@ def generate_launch_description():
     )
 
     # 라이다 드라이버
-    lidar_node = Node(
-        package='ydlidar_ros2_driver',
-        executable='ydlidar_ros2_driver_node',
-        name='ydlidar_ros2_driver_node',
-        output='screen',
-        emulate_tty=True,
-        parameters=[lidar_params],
-    )
+    #lidar_node = Node(
+    #    package='ydlidar_ros2_driver',
+    #    executable='ydlidar_ros2_driver_node',
+    #    name='ydlidar_ros2_driver_node',
+    #    output='screen',
+    #    emulate_tty=True,
+    #    parameters=[lidar_params],
+    #)
 
     # 라이다 TF (base_link → laser_frame)
-    lidar_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_tf_pub_laser',
-        arguments=['0', '0', '0.02', '0', '0', '0', '1',
-                   'base_link', 'laser_frame'],
-    )
+    #lidar_tf = Node(
+    #    package='tf2_ros',
+    #    executable='static_transform_publisher',
+    #    name='static_tf_pub_laser',
+    #    arguments=['0', '0', '0.02', '0', '0', '0', '1',
+    #               'base_link', 'laser_frame'],
+    #)
 
     # SLAM (라이다 뜨고 3초 후)
     slam_node = TimerAction(
@@ -115,12 +116,12 @@ def generate_launch_description():
     
 
     return LaunchDescription([
-        micro_ros_agent,
+        #micro_ros_agent,
         robot_state_publisher,
-        robot_node,
+        #robot_node,
         odometry_node,
-        lidar_node,
-        lidar_tf,
+        #lidar_node,
+        #lidar_tf,
         slam_node,
         slam_configure,
         slam_activate,
